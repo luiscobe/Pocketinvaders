6539d8011d7ebe83afe643a595f34c7837c46098
Luis Aznar Ferris <luazfe@floridauniversitaria.es>
2016-10-06 11:38:39 +0200

    parte 2

---
+++ pinvaders2/lib/ia.php
@@ -0,0 +1,26 @@
+<?php
+class ia
+{
+//propiedeades
+	public $fila = "3";
+	public $columna = "1";
+	//metodos
+public function getfila() {
+	return $this->fila;
+}
+public function getcolumna() {
+	return $this->columna;
+}	
+//setters
+public function setfila($fil) {
+	$this->fila =$fil;
+}
+public function setcolumna($col) {
+	$this->columna = $col;
+}
+//random
+public function RandomPos() {
+
+}
+}
+?>
